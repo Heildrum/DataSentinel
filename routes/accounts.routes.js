0d278c76e70289f2a7fa6444bc6ecad1e76d689d
Heildrum <hugomctapia@gmail.com>
2026-04-17 17:36:36 -0400

    Update accounts.routes.js
    correciones

--- routes/accounts.routes.js
+++ routes/accounts.routes.js
@@ -1,10 +1,10 @@
 
 const express = require("express");
 const router = express.Router();
-console.log("✅ accounts.routes.js cargado");
+console.log(" accounts.routes.js cargado");
 const cuentasService = require("../services/Cuentas");
 
-// ✅ Listar cuentas desde Cuentas.js
+//  Listar cuentas desde Cuentas.js
 router.get("/", (req, res) => {
   const accounts = cuentasService.getAccounts();
 
